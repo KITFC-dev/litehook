980713f706abed112324c6b276b4360ccb65a3ed
KITFC <kitfcdev@gmail.com>
2026-02-13 14:19:44 +0000

    feat: better logging maybe

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use tracing_subscriber::fmt::time::ChronoLocal;
 use anyhow::{Result, Ok};
 use litehook::{App, config};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt()
+        .with_timer(ChronoLocal::new("%Y-%m-%d %H:%M:%S".to_string()))
         .with_max_level(tracing::Level::INFO)
         .with_level(true)
         .with_target(false)
