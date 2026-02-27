593548bf530af3e313eb8219ad8c70bf494e0b58
KITFC <kitfcdev@gmail.com>
2026-02-27 18:26:37 +0000

    feat: implement some api endpoints for web api

--- src/api.rs
+++ src/api.rs
@@ -1,29 +1,30 @@
+use std::sync::Arc;
 use axum::{
-    Router,
+    Router, Json,
+    extract::{State, Path},
+    http::StatusCode,
     routing::{delete, get, post, put},
 };
 
-use crate::Server;
-use crate::config::Config;
+use crate::{Server};
+use crate::config::{Config, ListenerConfig};
 
-#[allow(unused)]
 pub struct Api {
     cfg: Config,
     router: Router,
-    server: std::sync::Arc<Server>,
+    server: Arc<Server>,
 }
 
 impl Api {
-    #[allow(unused)]
-    pub async fn new(cfg: Config, server: std::sync::Arc<Server>) -> anyhow::Result<Self> {
-        tracing::info!("starting web dashboard");
+    pub async fn new(cfg: Config, server: Arc<Server>) -> anyhow::Result<Self> {
+        tracing::info!("starting web api");
         let router = Router::new()
             .route("/listeners", get(Self::get_all_listeners))
             .route("/listeners", post(Self::add_listener))
             .route("/listeners/{id}", get(Self::get_listener))
             .route("/listeners/{id}", put(Self::update_listener))
             .route("/listeners/{id}", delete(Self::remove_listener))
-            .with_state(std::sync::Arc::clone(&server));
+            .with_state(Arc::clone(&server));
         Ok(Self {
             cfg,
             router,
@@ -31,7 +32,6 @@ impl Api {
         })
     }
 
-    #[allow(unused)]
     pub async fn run(&self) -> anyhow::Result<()> {
         let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", self.cfg.port)).await?;
 
@@ -39,27 +39,48 @@ impl Api {
             .with_graceful_shutdown(self.server.shutdown.clone().cancelled_owned())
             .await?;
 
-        tracing::info!("web dashboard stopped");
+        tracing::info!("web api stopped");
         Ok(())
     }
 
-    async fn get_all_listeners() {
-        unimplemented!()
+    async fn get_all_listeners(
+        State(_server): State<Arc<Server>>
+    ) -> (StatusCode, Json<Vec<String>>)
+    {
+        //! Return empty string vec for testing
+        (StatusCode::OK, Json(vec![]))
     }
 
-    async fn add_listener() {
-        unimplemented!()
+    async fn add_listener(
+        State(server): State<Arc<Server>>,
+        Json(body): Json<ListenerConfig>
+    ) -> StatusCode
+    {
+        server.add_listener(body).await;
+        StatusCode::OK
     }
 
-    async fn get_listener() {
-        unimplemented!()
+    async fn get_listener(
+        State(_server): State<Arc<Server>>,
+        Path(id): Path<String>
+    ) -> (StatusCode, String) {
+        //! Return empty string for testing
+        (StatusCode::OK, id.to_string())
     }
 
-    async fn update_listener() {
-        unimplemented!()
+    async fn update_listener(
+        State(server): State<Arc<Server>>,
+        Json(body): Json<ListenerConfig>
+    ) -> StatusCode {
+        server.update_listener(body).await;
+        StatusCode::OK
     }
 
-    async fn remove_listener() {
-        unimplemented!()
+    async fn remove_listener(
+        State(server): State<Arc<Server>>,
+        Path(id): Path<String>
+    ) -> StatusCode {
+        server.remove_listener(&id).await;
+        StatusCode::OK
     }
 }
